0b07db8660e666075549bdce6b3ca391b617cffb
BrenoRev <silvabreno462@gmail.com>
2022-01-07 22:16:34 -0300

    starting project

---
+++ src/test/java/com/dev/rev/prova/ProvaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.rev.prova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
